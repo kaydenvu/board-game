82038ab0cb3ecb389cc6ecc3a1b9859639a0f778
deltaflame <jakehung000@gmail.com>
2025-01-18 11:47:00 -0800

    Buy, Skip, Confirm, Pay Buttons defined, Jail & Player attributes involving jail defined.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 import pygame
-from tiles import board
+from tiles import board, INJAILPOS
 from enum import Enum
 
 # figure out git configs to work properly
@@ -52,7 +52,7 @@ roll = pygame.image.load("roll.png")
 buy = pygame.image.load("Buy button.png")
 skip = pygame.image.load("Skip button.png")
 confirm = pygame.image.load("Confirm Button.png")
-
+pay = pygame.image.load("Pay button.png")
 diePossiblities = [dice1, dice2, dice3, dice4, dice5, dice6]
 
 screenWidth = 500
@@ -70,6 +70,7 @@ BoardDisplay = pygame.sprite.Group()
 
 class Button(pygame.sprite.Sprite):
   mouseDown = False
+  showAlert = False
   def __init__(self, image, pos, scale, actionType, value = None, *groups):
     super().__init__(*groups)
     width = image.get_width()
@@ -80,6 +81,12 @@ class Button(pygame.sprite.Sprite):
     self.clicked = False
     self.actionType = actionType
     self.value = value
+  def resetButton(self):
+    global gameState
+    gameState = STATE.PLAY
+    self.clicked = False
+    Button.showAlert = False
+    buyButton.clicked = False
   def draw(self):
     action = False
     pos = pygame.mouse.get_pos()
@@ -90,7 +97,7 @@ class Button(pygame.sprite.Sprite):
     screen.blit(self.image, self.rect)
     return action
   def update(self):
-    global playerAmount, gameState
+    global playerAmount, gameState, lastPlayer, tile
     if self.actionType == "num players":
       if self.draw():
         playerAmount = self.value
@@ -104,18 +111,47 @@ class Button(pygame.sprite.Sprite):
         self.kill()
         if len(players) == playerAmount:
           gameState = STATE.PLAY
-        print(players)
+          print(players)
     if self.actionType == "roll dice":
       global diceRolling
       if self.draw():
         diceRolling = True
         pygame.time.set_timer(rollingDice, Die.rollTime)  
+        self.resetButton()
     if self.actionType == "buy":
       self.draw()
+      if self.clicked and not Button.mouseDown:
+        if lastPlayer.money>= tile.value:
+          lastPlayer.money -= tile.value
+          self.resetButton()
+        else:
+          Button.showAlert = True
+        if Button.showAlert:
+          drawText("Not enough money", V(250, 300))
     if self.actionType == "skip":
       self.draw()    
+      if self.clicked and not Button.mouseDown:
+        self.resetButton()
     if self.actionType == "confirm":
       self.draw()
+      if self.clicked and not Button.mouseDown:
+        if tile.tileType == "Go To Jail":
+          lastPlayer.position = 10
+          lastPlayer.inJail = True
+        self.resetButton()
+    if self.actionType == "pay":
+      self.draw()
+      if self.clicked and not Button.mouseDown:
+        if lastPlayer.money >= 50:
+          lastPlayer.money -= 50
+          lastPlayer.inJail = False
+          lastPlayer.jailTime = 0
+          self.resetButton()
+        else:
+          Button.showAlert = True
+        if Button.showAlert:
+          drawText("Not enough money", V(250, 300))
+
 for idx, num in enumerate([num2, num3, num4, num5], 2):
   Button(num, V(5 + (idx - 2) * 125 ,100), 0.5, "num players", idx, PlayerNumUI)
 for idx, num in enumerate([num6,num7,num8], 6):
@@ -134,6 +170,8 @@ class Player():
     self.position=0
     self.money=1500
     self.properties=[]
+    self.inJail = False
+    self.jailTime= 0
   def __repr__(self):
     return self.piece.name
     
@@ -160,6 +198,8 @@ def printPieces():
 
 players=[]
 player = None
+lastPlayer = None
+tile = board[0]
     
 random.shuffle(players)
 print("Order:", players)
@@ -185,17 +225,31 @@ class Die():
 die1 = Die((170, 170))
 die2 = Die((330, 330))
 rollButton = Button(roll, V(215, 215), 0.5, "roll dice")
-buyButton = Button(buy,V(125,200), 0.5, "buy")
+buyButton = Button(buy, V(125,200), 0.5, "buy")
 skipButton = Button(skip, V(300, 200), 0.5, "skip")
-confirmButton = Button(confirm, V(215,200), 0.5, "confirm")
+confirmButton = Button(confirm, V(195,215), 0.5, "confirm")
+payButton = Button(pay, V(300,215), 0.5, "pay")
 
 def diceRoll():
   return (random.randint(1,6), random.randint(1,6))
   
-def move(player, roll):
-  player.position += roll
+def move(player, d1, d2):
+  roll = d1 + d2
+  if player.inJail:
+    if d1 == d2:
+      player.inJail = False
+      player.position+=roll
+      player.jailTime = 0
+    player.jailTime += 1
+    if player.jailTime == 3:
+      player.money -= 50
+      player.position += roll
+      player.inJail = False
+      player.jailTime = 0
+  else: player.position += 10
   if player.position >= 40:
     player.position -= 40
+    player.money+=200
   return player.position
 
 font = pygame.font.Font('Rubik.ttf', 32)
@@ -214,25 +268,37 @@ def printBoard():
       print()
 
 def updateBoard():
-  global player, gameState
+  global player, gameState, lastPlayer, tile
+  lastTurn = turn - 1
+  lastPlayer = players[lastTurn]
+  tile = board[lastPlayer.position]
   screen.blit(boardImage,(0,0))
   if len(players)>=1 and not firstTurn:
-    lastTurn = turn - 1
-    lastPlayer = players[lastTurn]
     drawText(lastPlayer.piece.name,V(350,100))
     drawText("$" + str(lastPlayer.money), V(350, 150))
+    # if player.inJail:
+    #   drawText(lastPlayer.piece.name + " has been in jail for " + str(lastPlayer.jailTime) + " turns.", (250, 350))
     drawText(lastPlayer.piece.name + " landed on ", (250, 350))
-    drawText(board[lastPlayer.position].name, (250, 400))
+    drawText(tile.name, (250, 400))
   for player in players:
-    screen.blit(player.piece.image, board[player.position].position)
+    if board[player.position].name == "Jail" and player.inJail: 
+      screen.blit(player.piece.image, INJAILPOS)
+    else:
+      screen.blit(player.piece.image, board[player.position].position)
   if diceRolling:
     die1.draw()
     die2.draw()
   elif gameState == STATE.PLAY:
     rollButton.update()
+    if lastPlayer.inJail:
+      payButton.update()
   elif gameState == STATE.PLAY_CHOOSE:
-    buyButton.update()
-    skipButton.update()
+    if tile.tileType == "Property":
+      drawText("$" + str(tile.value), V(160, 185))
+      buyButton.update()
+      skipButton.update()
+    else:
+      confirmButton.update()
   pygame.display.update()
   
 GameRunning=True
@@ -268,7 +334,7 @@ while GameRunning:
         diceRolling = False
         rollButton.clicked = False
         firstTurn = False
-        move(player, die1.value+die2.value)
+        move(player, die1.value, die2.value)
         print(player.piece.name, die1.value, die2.value, board[player.position].name)
         updateBoard()
         gameState = STATE.PLAY_CHOOSE
